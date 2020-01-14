ade28e0d8d562c9043001412e210785064b034f1
Alexandr Shevqnov <shevqnov@gmail.com>
2020-01-14 18:08:54 +0500

    lint

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
 
-class Card extends React.Component {
-    componentDidMount = () => {
-      // eslint-disable-next-line no-console
-      console.log(this.props);
-    }
+const Card = ({ title, text, image }) => (
+  <div>
+    <h3>{title}</h3>
+    <p>{text}</p>
+    <img src={image} alt="test" />
+  </div>
+);
 
-    render() {
-      const { title, text, image } = this.props;
-      return (
-        <div>
-          <h3>{title}</h3>
-          <p>{text}</p>
-          <img src={image} alt="test" />
-        </div>
-      );
-    }
-}
 
 export default Card;
