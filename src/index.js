3ad526acb64d5f96430d9ec2e81e09efdc267414
Alexandr Shevqnov <shevqnov@gmail.com>
2020-01-20 17:32:24 +0500

    capitalize a in App

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { render } from 'react-dom';
 import React from 'react';
-import App from './app';
+import App from './App';
 
 render(React.createElement(App), document.getElementById('root'));
