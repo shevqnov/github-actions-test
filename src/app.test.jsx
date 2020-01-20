456a1624cb12ee5242a909f1e17b233e78af0c6f
Alexandr Shevqnov <shevqnov@gmail.com>
2020-01-20 15:26:32 +0500

    rm app test

--- src/app.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { mount } from 'enzyme';
-import App from './app.tsx';
-
-test('app should be rendered', () => {
-  const wrapper = mount(<App />);
-  expect(wrapper).toMatchSnapshot();
-});
